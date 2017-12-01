bc8dda0a7d3c7624e4b8c87ce1d2470cd32aca4e
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-11-30 18:27:53 -0600

    Removed folder

--- config/database.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {
-        mongoURI: 'mongodb://admin:admin123*@ds113660.mlab.com:13660/vandystemtutoring'
-    }
-} else {
-    module.exports = {
-        mongoURI: 'mongodb://localhost/stemtutoring'
-    }
-}
